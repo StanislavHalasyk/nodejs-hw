f1b6fde53f1b849308927d58e3b2a6d8e4d30225
halasyk_stanislav <galasik_stanislav@icloud.com>
2026-01-17 22:04:15 +0200

    hw

---
+++ src/models/note.js
@@ -0,0 +1,43 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true, // прибирає пробіли на початку та в кінці
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      required: true,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+    onDuty: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+export const Note = model('Note', noteSchema);
