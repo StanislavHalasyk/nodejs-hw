f39cbfb71d07def1a672a2881eba7e0407b45a7f
halasyk_stanislav <galasik_stanislav@icloud.com>
2026-01-25 15:37:13 +0200

    HW

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -14,7 +14,7 @@ import {
   noteIdSchema,
   createNoteSchema,
   updateNoteSchema,
-} from '../validation/notesSchemas.js';
+} from '../validations/notesValidation.js';
 
 const router = Router();
 
