cfc15ccc7265efc547dd85a31cfa8ac42021f195
halasyk_stanislav <galasik_stanislav@icloud.com>
2026-01-25 09:15:23 +0200

    HW

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'dotenv/config';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import notesRouter from './routes/notesRoutes.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import notesRouter from './routes/notesRoutes.js';
 
 const app = express();
-
 const PORT = process.env.PORT ?? 3000;
 
+// Middleware
 app.use(logger);
 app.use(
   express.json({
@@ -23,11 +22,19 @@ app.use(
 );
 app.use(cors());
 
+// Routes (без префікса!)
 app.use(notesRouter);
 
+// 404 handler
 app.use(notFoundHandler);
+
+// Celebrate errors handler
+app.use(errors());
+
+// Custom error handler
 app.use(errorHandler);
 
+// DB + Server
 await connectMongoDB();
 
 app.listen(PORT, () => {
